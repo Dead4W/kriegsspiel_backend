94313ef7ee1a79ce04fa91b91cec9e9b686dac2e
Ilya Zedgenizov <dead4w@gmail.com>
2026-01-25 11:20:57 +0400

    Add sentry middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+        \Sentry\Laravel\Http\SetRequestMiddleware::class,
+        \Sentry\Laravel\Http\SetRequestIpMiddleware::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
