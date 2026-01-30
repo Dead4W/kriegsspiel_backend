73bdcf305f5085bc595ed3e71357e7d2f1080d9a
Ilya Zedgenizov <dead4w@gmail.com>
2026-01-30 11:33:46 +0400

    Fix DisableCors

--- app/Http/Middleware/DisableCors.php
+++ app/Http/Middleware/DisableCors.php
@@ -3,18 +3,37 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class DisableCors
 {
 
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-            ->header('Accept', 'application/json');
+        $response = $next($request);
+
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => '*',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Headers'     => 'X-Requested-With,Content-Type,X-Token-Auth,Authorization',
+            'Accept'                           => 'application/json',
+        ];
+
+        // Illuminate\Http\Response
+        if (method_exists($response, 'header')) {
+            foreach ($headers as $k => $v) {
+                $response->header($k, $v);
+            }
+        }
+        // Symfony\Component\HttpFoundation\Response (StreamedResponse, BinaryFileResponse, etc)
+        elseif ($response instanceof Response) {
+            foreach ($headers as $k => $v) {
+                $response->headers->set($k, $v);
+            }
+        }
+
+        return $response;
     }
 
 }
