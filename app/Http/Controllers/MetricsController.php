9fb17127d0f84f6c6fbfc3d9e04fb31c5085e22f
Ilya Zedgenizov <dead4w@gmail.com>
2026-03-11 20:31:33 +0400

    Fix metrics

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -25,31 +25,8 @@ class MetricsController extends Controller
 
         // Current WebSocket connections (gauge)
         $connectionsCount = Connection::count();
-        $lines[] = '# HELP websocket_connections_current Current number of active WebSocket connections';
-        $lines[] = '# TYPE websocket_connections_current gauge';
         $lines[] = "kriegsspiel.websocket_connections_current {$connectionsCount}";
 
-        // Connections by team (optional breakdown)
-        $connectionsByTeam = Connection::query()
-            ->selectRaw('team, count(*) as count')
-            ->groupBy('team')
-            ->get();
-
-        if ($connectionsByTeam->isNotEmpty()) {
-            $lines[] = '';
-            $lines[] = '# HELP websocket_connections_by_team Current WebSocket connections grouped by team';
-            $lines[] = '# TYPE websocket_connections_by_team gauge';
-            foreach ($connectionsByTeam as $row) {
-                $team = $this->escapeLabelValue((string) $row->team);
-                $lines[] = "websocket_connections_by_team{team=\"{$team}\"} {$row->count}";
-            }
-        }
-
         return implode("\n", $lines) . "\n";
-    }
-
-    private function escapeLabelValue(string $value): string
-    {
-        return str_replace(['\\', "\n", '"'], ['\\\\', '\\n', '\\"'], $value);
     }
 }
