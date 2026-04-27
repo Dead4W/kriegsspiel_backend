a56a938a4dd2370870aea48430d5a302fb8c1d0b
Ilya Zedgenizov <dead4w@gmail.com>
2026-04-27 12:18:39 +0400

    Multithread backend

--- app/Console/Commands/SocketStartCommand.php
+++ app/Console/Commands/SocketStartCommand.php
@@ -7,18 +7,16 @@ use App\Socket\Callbacks\SocketOnMessageCallback;
 use App\Socket\Callbacks\SocketOnOpenCallback;
 use App\Socket\Callbacks\SocketOnStartCallback;
 use Illuminate\Console\Command;
+use OpenSwoole\Constant;
 use OpenSwoole\Server as ServerAlias;
 use OpenSwoole\WebSocket\Server;
-use OpenSwoole\WebSocket\Frame;
-use OpenSwoole\Constant;
-use Sentry\SentrySdk;
-use Sentry\Tracing\TransactionContext;
 
 class SocketStartCommand extends Command
 {
     public $signature = 'socket:start
                     {--host= : The IP address the server should bind to [default: "0.0.0.0"]]}
                     {--port= : The port the server should be available on [default: "9501"]}
+                    {--workers= : Number of OpenSwoole worker processes [default: CPU count, minimum 2]}
     ';
 
     public $description = 'Start the Socket server';
@@ -26,25 +24,34 @@ class SocketStartCommand extends Command
     public function handle() {
         ini_set('memory_limit', '512M');
 
+        $workerNum = $this->getWorkerNum();
+
         $server = new Server(
             host: $this->option('host') ?? "0.0.0.0",
             port: $this->option('port') ?? 9501,
-            mode: ServerAlias::SIMPLE_MODE,
+            mode: ServerAlias::POOL_MODE,
             sockType: Constant::SOCK_TCP
         );
 
         $server->set([
-            'input_buffer_size' => 64 * 1024 * 1024,
-            'buffer_output_size' => 64 * 1024 * 1024,
+            'worker_num' => $workerNum,
+            'open_cpu_affinity' => true,
+
+            'max_request' => 100,
+
+            'input_buffer_size' => 128 * 1024 * 1024,
+            'buffer_output_size' => 128 * 1024 * 1024,
             'socket_buffer_size' => 128 * 1024 *1024,
 
-            'kernel_socket_send_buffer_size' => 64 * 1024 * 1024,
-            'kernel_socket_recv_buffer_size' => 64 * 1024 * 1024,
+            'kernel_socket_send_buffer_size' => 128 * 1024 * 1024,
+            'kernel_socket_recv_buffer_size' => 128 * 1024 * 1024,
 
-            'package_max_length' => 64 * 1024 * 1024,
+            'package_max_length' => 128 * 1024 * 1024,
 
             'websocket_compression' => true,
         ]);
+
+        $this->info("Starting WebSocket server with {$workerNum} workers...");
 
         $server->on("Start", new SocketOnStartCallback($this->output));
 
@@ -55,6 +62,18 @@ class SocketStartCommand extends Command
         $server->on('Disconnect', new SocketOnCloseCallback($this->output));
 
         $server->start();
+    }
+
+    private function getWorkerNum(): int
+    {
+        $option = $this->option('workers');
+        if (is_numeric($option) && (int) $option > 0) {
+            return (int) $option;
+        }
+
+        $cpuNum = function_exists('swoole_cpu_num') ? swoole_cpu_num() : 2;
+
+        return max(2, (int) $cpuNum);
     }
 
 }
